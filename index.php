e00cd57843f961c92ee827f800aadf2cecf6d1dd
Harry Houghton <h.oscar3@gmail.com>
2013-09-17 22:41:58 -0400

    .css worked, now adding images (also a few updates to the CSS and HTML)

--- index.php
+++ index.php
@@ -10,13 +10,16 @@
 
 <body>
 	
-	<h1>A Brief History of<br><span>Harry Houghton</span></h1>
+	<h1>A Brief History of<br><span id="name">Harry Houghton</span></h1>
+
+	<br>&nbsp;<br>
 
 	<p>To Begin: My name is Henry Houghton, III. This is what I figure it says for my course enrollment information too, in case there is any confusion about the title of this bio. I go by Harry though, and always have! I currently work at Pearson Education in Back Bay, and although I end all of my emails with "Thanks, Harry", most people still call me Henry because that's what how my email address lists me. I am a project manager for creating online course portals for college classes, and although I have been at Pearson for 6+ months and now know how to complete a project, I still end a project unsure what it is I did.</p>
 	<p>I went to Brown undergrad and taught English in China for a year and have a Hubway membership (and a helmet, safety first) and I like martinis but still hate olives and I love to visit my sister in London but otherwise miss her and my '96 Corolla was recently broken into but only so the tail lights could be stolen (hot commodity, I guess?) and one of my favorite books is <em>Deliverance</em> by James Dickey &ndash; you should read it.</p>
 	<p>Other than that, I'm excited to take this course! I never took much (read: any) computer science courses in college, but I needed to use it periodically in the jobs I've had and really enjoyed it, so I'm trying to keep building my knowledge base. I took Java II with Prof. Henry Leitner last year, good fun. I have no doubt this will be just as fun! Oh and the code editor I'm using is Sublime Text 2, though I'll also occasionally use DreamWeaver if I do any of this while at work (please don't tell).</p>
 	<p>There's always more, but that should do well as a small bio. If you need to contact me for any reason, my email address is h.oscar3@gmail.com. Looking forward to the next class!</p>
+	<br>&nbsp;<br>
 	<hr>
-	<p id="end">This ends the Brief History of Harry Houghton</p>
+	<p id="end">This ends <em>A Brief History of Harry Houghton</em><br>Meanwhile, here's a picture of <a href="withbull.php">me with a bull</a></p>
 
 </body>
---
+++ withbull.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>
+	<base href="http://p1.rubburduck.biz/">
+	<style type="text/css">
+	body {margin: 20% 20%;}
+	</style>
+</head>
+<body>
+	<img src="images/bull.JPG">
+</body>
